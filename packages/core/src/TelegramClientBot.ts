4dabb0e4219e1c155e2ad7cdcf5f03b221bc3bc8
Maksim C <jstars.tech@gmail.com>
2024-12-02 15:11:51 +0300

    Revert "Refactor: improve error logging for grouped event processing"
    This reverts commit c9ae503c1c69eda78df780366ebd0635d2745cb6.

--- packages/core/src/TelegramClientBot.ts
+++ packages/core/src/TelegramClientBot.ts
@@ -158,7 +158,7 @@ export default class TelegramClientBot extends EventEmitter {
         try {
             await setTimeout(5000, null, { signal: ac.signal });
         } catch (e) {
-            this.logger.error('Error processing grouped event', { error: e, groupedId: groupedId });
+            this.logger.debug('New grouped event received', { groupedId: groupedId });
             return;
         }
 
