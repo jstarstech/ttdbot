32592984ab39bd346de3bf8c3dc8be77cf4a491f
Maksim C <jstars.tech@gmail.com>
2024-12-02 14:50:52 +0300

    Refactor: improve error handling in main initialization process

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -3,13 +3,16 @@ import DiscordClient from './DiscordClient.js';
 import TelegramClientBot from './TelegramClientBot.js';
 import logger from './Logger.js';
 
-(async () => {
-    logger.info(`Data directory ${config.dataDir}`);
+logger.info(`Data directory ${config.dataDir}`);
 
+try {
     const telegramClientBot = new TelegramClientBot(config);
     await telegramClientBot.init();
+
     const discordClient = new DiscordClient(config);
     await discordClient.init();
 
     telegramClientBot.addListener('newMessage', discordClient.postMessage.bind(discordClient));
-})();
+} catch (e) {
+    logger.error(e);
+}
